49b53dab8cf5a952f3b863772a472a245908cd54
Pierre Possamai Da Silva <pierre.silva@satc.edu.br>
2024-10-02 18:53:27 -0300

    Primeira aula

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import ProdutoItem from "@/components/produto/produtoItem";
+import { produtos } from "@/core";
+
+export default function Home() {
+  return (
+    <div className="flex gap-5 flex-wrap">
+      <ProdutoItem produto={produtos[0]} />
+      <ProdutoItem produto={produtos[1]}/>
+      <ProdutoItem produto={produtos[2]}/>
+      <ProdutoItem produto={produtos[18]}/>
+    </div>
+    
+  );
+}
